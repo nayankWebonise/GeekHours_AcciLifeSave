f0d4a9d63b80be03d40b4655dc8f9ddef58156c8
vijayraj <vijayraj.nathe@weboniselab.com>
2015-10-17 15:42:16 +0530

    change icon size

--- grails-app/assets/javascripts/component/dashboard-view.js
+++ grails-app/assets/javascripts/component/dashboard-view.js
@@ -72,8 +72,8 @@ var DashBoard = React.createClass({
                 title: 'My Car',
                 icon: {
                     iconUrl: '/assets/car.png',
-                    iconSize: [50, 50], // size of the icon
-                    iconAnchor: [40, 40], // point of the icon which will correspond to marker's location
+                    iconSize: [30, 30], // size of the icon
+                    iconAnchor: [20, 20], // point of the icon which will correspond to marker's location
                     popupAnchor: [0, -25], // point from which the popup should open relative to the iconAnchor
                     className: 'dot'
                 }
